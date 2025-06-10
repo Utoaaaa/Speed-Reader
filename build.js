ec534df853b3d6cb6ed8c0bc715c7ba35e51ebdb
Utoa <nick940624@gmail.com>
2025-06-11 01:56:28 +0800

    新增構建腳本以確保 dist 目錄存在，並處理 index.html 及複製必要檔案

--- build.js
+++ build.js
@@ -1,5 +1,23 @@
 require('dotenv').config();
 const esbuild = require('esbuild');
+const fs = require('fs');
+const path = require('path');
+
+// 確保 dist 目錄存在
+const distDir = path.join(__dirname, 'dist');
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
+// 處理 index.html：讀取、修改路徑並寫入 dist
+let indexContent = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
+indexContent = indexContent.replace(/href="dist\//g, 'href="').replace(/src="dist\//g, 'src="');
+fs.writeFileSync(path.join(distDir, 'index.html'), indexContent);
+
+// 複製其他必要的根目錄檔案到 dist
+fs.copyFileSync(path.join(__dirname, 'AestheticFluidBg.min.js'), path.join(distDir, 'AestheticFluidBg.min.js'));
+fs.copyFileSync(path.join(__dirname, 'favicon.svg'), path.join(distDir, 'favicon.svg'));
+
 
 esbuild.build({
   entryPoints: ['script.js'],
